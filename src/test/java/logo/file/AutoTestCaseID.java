11f14ec13ae13dcaf8353893e4735bae139a6cff
xumeina <xumeina@etiantian.com>
2017-05-24 18:29:03 +0800

    Initial commit

---
+++ src/test/java/logo/file/AutoTestCaseID.java
@@ -0,0 +1,173 @@
+package logo.file;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import jxl.read.biff.BiffException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
+
+/**
+ * Created by LiuChuanan on 2017/5/24.
+ */
+public class AutoTestCaseID {
+    ElementExist el = new ElementExist();
+    static AutoTestExcelFile ft = new AutoTestExcelFile();
+    SlidePage sp = new SlidePage();
+    public void TestCaseId(AppiumDriver driver, String id) throws InterruptedException ,BiffException, IOException{
+        int i,j,k,l,m,n,o,p,q;
+        for(i=0; i<ft.ReadContent().size(); i++){
+            if(ft.ReadContent().get(i).contains(id)){
+                for(j=0; j<ft.ReadTitle().size(); j++){
+                    if(ft.ReadTitle().get(j).contains("定位方式")){
+                        break;
+                    }
+                }
+                String caseidLocation = ft.ReadTitleContent(i+1,j);
+                for(k=0; k<ft.ReadTitle().size(); k++){
+                    if(ft.ReadTitle().get(k).contains("控件元素")){
+                        break;
+                    }
+                }
+                String caseidElement = ft.ReadTitleContent(i+1,k);
+                for(l=0; l<ft.ReadTitle().size(); l++){
+                    if(ft.ReadTitle().get(l).contains("操作方法")){
+                        break;
+                    }
+                }
+                String caseidOperationMethod = ft.ReadTitleContent(i+1,l);
+                for(m=0; m<ft.ReadTitle().size(); m++){
+                    if(ft.ReadTitle().get(m).contains("测试数据")){
+                        break;
+                    }
+                }
+                String caseidTestData = ft.ReadTitleContent(i+1,m);
+                for(n=0; n<ft.ReadTitle().size(); n++){
+                    if(ft.ReadTitle().get(n).contains("验证数据")){
+                        break;
+                    }
+                }
+                String caseidVerifyData = ft.ReadTitleContent(i+1,n);
+                for(o=0; o<ft.ReadTitle().size(); o++){
+                    if(ft.ReadTitle().get(o).contains("延迟时间")){
+                        break;
+                    }
+                }
+                String sleepTime = ft.ReadTitleContent(i+1,o);
+                if(caseidLocation.equals("By.xpath")) {
+                    if (caseidOperationMethod.equals("sendkeys")) {
+                        el.waitForElementByXpath(caseidElement, driver);
+                        driver.findElement(By.xpath(caseidElement)).sendKeys(caseidTestData);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    } else if (caseidOperationMethod.equals("click")) {
+                        el.waitForElementByXpath(caseidElement, driver);
+                        driver.findElement(By.xpath(caseidElement)).click();
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    } else if (caseidOperationMethod.equals("swipedown")) {
+                        el.waitForElementByXpath(caseidElement, driver);
+                        sp.Down_Page(caseidElement, driver);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    }
+                }else  if(caseidLocation.equals("By.id")) {
+                    if (caseidOperationMethod.equals("sendkeys")) {
+                        el.waitForElementByID(caseidElement, driver);
+                        driver.findElement(By.id(caseidElement)).sendKeys(caseidTestData);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    } else if (caseidOperationMethod.equals("click")) {
+                        el.waitForElementByID(caseidElement, driver);
+                        driver.findElement(By.id(caseidElement)).click();
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    } else if (caseidOperationMethod.equals("swipedown")) {
+                        el.waitForElementByID(caseidElement, driver);
+                        sp.Down_Page(caseidElement, driver);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    }else if (caseidOperationMethod.equals("DownPage")) {
+                        el.waitForElementByID(caseidElement, driver);
+                        sp.DownPage(caseidElement, driver);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    }
+                }else if(caseidLocation.equals("By.name")) {
+                    if (caseidOperationMethod.equals("sendkeys")) {
+                        el.waitForElementByName(caseidElement, driver);
+                        driver.findElement(By.name(caseidElement)).sendKeys(caseidTestData);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    } else if (caseidOperationMethod.equals("click")) {
+                        el.waitForElementByName(caseidElement, driver);
+                        driver.findElement(By.name(caseidElement)).click();
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    } else if (caseidOperationMethod.equals("press")) {
+                        el.waitForElementByName(caseidElement, driver);
+                        WebElement e = driver.findElement((By.name(caseidElement)));
+                        TouchAction action = new TouchAction(driver);
+                        action.press(e).waitAction(5000);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    }
+                }else if(caseidLocation.equals("By.driver")) {
+                    if (caseidOperationMethod.equals("swipetoup")) {
+                        //el.waitForElementByName(caseidElement, driver);
+                        sp.swipeToUp(Integer.parseInt(caseidElement),driver);
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    }
+                }else if(caseidLocation.equals("By.name")){
+                if("pass".equals(el.waitForElementByNameSkip(caseidElement,driver))){
+                    if(caseidOperationMethod.equals("click")){
+                        driver.findElement(By.name(caseidElement)).click();
+                        if (sleepTime != null && sleepTime.length() != 0) {
+                            String stepTime = sleepTime.substring(0, sleepTime.indexOf("."));
+                            Thread.sleep(Integer.parseInt(stepTime));
+                        }
+                    }else if("failed".equals(el.waitForElementByNameSkip(caseidElement,driver)))){
+                            continue;
+                    }
+                }
+            }
+            if(caseidVerifyData !=null && caseidVerifyData.length()!=0){
+                    el.waitForElementByName(caseidVerifyData,driver);
+            }
+            for(p=0; p <ft.ReadTitle().size(); P++){
+                if(ft.ReadTitle().get(p).contains("测试结果")){
+                    break;
+                }
+            }
+            ft.WriteTitleContent(i+1,(short)p);
+            }else {
+
+            }
+        }
+        Thread.sleep(3000);
+    }
+}
---
+++ src/test/java/logo/file/AutoTestExcelFile.java
@@ -0,0 +1,207 @@
+package logo.file;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import jxl.read.biff.BiffException;
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.eclipse.jetty.util.IO;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import sun.security.krb5.Config;
+
+import javax.xml.bind.Element;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+/**
+ * Created by LiuChuanan on 2017/5/24.
+ */
+public class AutoTestExcelFile {
+    private POIFSFileSystem fileSystem;
+    private HSSFWorkbook workbook;
+    private HSSFSheet sheet;
+    private HSSFRow row;
+    private HSSFCell cell;
+    ElementExist el = new ElementExist();
+    public String[] readExcelTitleContent(InputStream is){
+        try{
+            fileSystem = new POIFSFileSystem(is);
+            workbook = new HSSFWorkbook(fileSystem);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        sheet = workbook.getSheet(Config.getInstance().getCfg("My"));
+        row = sheet.getRow(0);
+        int colNum = row.getPhysicalNumberOfCells();
+        String[] title = new String[colNum];
+        for (int i=0; i < colNum; i+=){
+            title[i] = getStringCellValue(row.getCell((short)i));
+        }
+        return title;
+    }
+    public Map<Integer,String> readExcelContent(InputStream is){
+        Map<Integer, String> content = new HashMap<Integer, String>();
+        String str = "";
+        try{
+            fileSystem = new POIFSFileSystem(is);
+            workbook = new HSSFWorkbook(fileSystem);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        sheet = workbook.getSheet(Config.getInstance().getCfg("My"));
+        int rowNum = sheet.getLastRowNum();
+        row = sheet.getRow(0);
+        int colNum = row.getPhysicalNumberOfCells();
+        String[] title = new String[colNum];
+        for (int i=1; i < colNum; i+=){
+            row = sheet.getRow(i);
+            int j =0;
+            while (j<colNum){
+                str+ = getStringCellValue(row.getCell((short)j)).trim()+"-";
+                j++;
+            }
+            content.put(i,str);
+            str = "";
+        }
+        return content;
+    }
+    public String getStringCellValue(HSSFCell cell){
+        String strCell = "";
+        if(cell==null){
+            return "";
+        }else {
+            switch (cell.getCellType()){
+                case HSSFCell.CELL_TYPE_STRING:
+                    strCell = cell.getStringCellValue();
+                    break;
+                case HSSFCell.CELL_TYPE_NUMERIC:
+                    String strCellNumber = String.valueOf(cell.getNumericCellValue());
+                    DecimalFormat df= new DecimalFormat("0");
+                    strCell = df.format(cell.getNumericCellValue());
+                    break;
+                case HSSFCell.CELL_TYPE_BOOLEAN:
+                    strCell = String.valueOf(cell.getBooleanCellValue());
+                    break;
+                case HSSFCell.CELL_TYPE_BLANK:
+                    strCell = "";
+                    break;
+                default:
+                    strCell = "";
+                    break;
+            }
+            if (strCell.equals("")||strCell==null){
+                return "";
+            }
+            return strCell;
+        }
+    }
+    private String getDateCellValue(HSSFCell cell){
+        String result = "";
+        try{
+            int cellType = cell.getCellType();
+            if(cellType==HSSFCell.CELL_TYPE_NUMERIC){
+                Date date = cell.getDateCellValue();
+                result = (date.getYear()+1900)+"-"+(date.getMonth())+"-"+(date.getDate());
+            }else if(cellType==HSSFCell.CELL_TYPE_STRING){
+                String date = getStringCellValue(cell);
+                result = date.replaceAll("[年月]","-").replace("日","").trim();
+            }else if(cellType==HSSFCell.CELL_TYPE_BLANK){
+                result="";
+            }
+        }catch (Exception e){
+            System.out.println("日期格式不正确");
+            e.printStackTrace();
+        }
+        return result;
+    }
+    public List<String> ReadTitle() throws FileNotFoundException{
+        List<String> list = new ArrayList<String>();
+        InputStream is= new FilterInputStream(Config.getInstance().getCfg("filePath"));
+        AutoTestExcelFile excelReader = new AutoTestExcelFile();
+        String [] title =  excelReader.readExcelTitleContent(is);
+        System.out.println("获得Excel表格的标题");
+        for (String s:title){
+            list.add(s);
+        }
+        return list;
+    }
+    public List<String> ReadContent() throws FileNotFoundException{
+        List<String> list = new ArrayList<String>();
+        AutoTestExcelFile excelReader = new AutoTestExcelFile();
+        InputStream is2 = new FilterInputStream(Config.getInstance().getCfg("filePath"));
+        Map<Integer,  String> map = excelReader.readExcelContent(is2);
+        for (int i =1; i<map.size(); i+=){
+            list.add(map.get(i));
+        }
+        return list;
+    }
+    public String ReadTitleContent(int i, int j) throws BiffException,IOException{
+        InputStream is= new FilterInputStream(Config.getInstance().getCfg("filePath"));
+        try{
+         fileSystem = new POIFSFileSystem(is);
+         workbook = new HSSFWorkbook(fileSystem);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        sheet = workbook.getSheet(Config.getInstance().getCfg("My"));
+        row = sheet.getRow(i);
+        String content = getStringCellValue(row.getCell((short) j));
+        return  content;
+    }
+    private void saveWorkBook(HSSFWorkbook wb){
+        try{
+            FileOutputStream fileOut= new FileOutputStream(Config.getInstance().getCfg("filePath"));
+            wb.write(fileOut);
+        }catch (FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+    private HSSFCell getCell(HSSFSheet sheet, int rowIndex, short columnIndex){
+        HSSFRow row = sheet.getRow(rowIndex);
+        if (row==null){
+            row = sheet.createRow(rowIndex);
+        }
+        HSSFCell cell = row.getCell(columnIndex);
+        if (cell==null){
+            cell = row.createCell((short)columnIndex);
+        }
+        return cell;
+    }
+    public void WriteTitleContent(int i, short j) throws BiffException,IOException{
+        try{
+            InputStream is= new InputStream(Config.getInstance().getCfg("filePath"));
+            fileSystem = new POIFSFileSystem(is);
+            workbook =new HSSFWorkbook(fileSystem);
+            is.close();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        sheet = workbook.getSheet(Config.getInstance().getCfg("My"));
+        row = sheet.getRow(i);
+        HSSFCell cell = getCell(sheet,i,j);
+        HSSFRichTextString hts = new HSSFRichTextString(el.result);
+        cell.getCellValue(hts);
+        saveWorkBook(workbook);
+    }
+    public void SetContentInit(short j) throws  BiffException, IOException{
+        try{
+            InputStream is= new FileInputStream(Config.getInstance().getCfg("filePath"));
+            Map<Integer,String> map = readExcelContent(is);
+            for (int k =1; k <map.size(); k+=){
+                sheet = workbook.getSheet(Config.getInstance().getCfg("My"));
+                row = sheet.getRow(k);
+                HSSFCell cell= getCell(sheet,k,j);
+                HSSFRichTextString hts = new HSSFRichTextString("");
+                cell.setCellValue(hts);
+
+            }
+            saveWorkBook(workbook);
+        }catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+ }
